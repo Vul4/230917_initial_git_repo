96062cac87b3ecbcd95a302cb086dac9dbd05916
Vul4 <r10632011@ntu.edu.tw>
2023-09-19 16:36:58 +0800

    edit_try2

--- 2ndtry.R
+++ 2ndtry.R
@@ -1,1 +1,9 @@
 #2nd try
+
+iris$color<-ifelse(iris$Species=="setosa","purple",ifelse(iris$Species=="vercicolor", "blue","pink") )
+head(iris)
+tail(iris)
+iris<-iris[order(iris$Sepal.Width, decreasing = T),]
+vercicolor<-subset(iris, Species == "vercicolor")
+
+#edit
